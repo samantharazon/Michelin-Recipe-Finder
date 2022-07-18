72f78afc947571dd989c0e45e23268fee728c0c0
samantharazon <samantharazon2@gmail.com>
2022-07-18 02:13:49 -0400

    Initial Commit

---
+++ recipeProject/recipeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipeProjectApp.swift
+//  recipeProject
+//
+//  Created by Samantha Razon on 7/18/22.
+//
+
+import SwiftUI
+
+@main
+struct recipeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
